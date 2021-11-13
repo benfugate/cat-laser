426ed2b64ff5d605f43f98351012d496ea5d12c9
benfugate <benjamin_fugate13@yahoo.com>
2021-11-12 21:45:45 -0500

    change where laser is set

--- src/laser.py
+++ src/laser.py
@@ -54,7 +54,7 @@ class Laser:
                 time.sleep(self.move_delay_seconds)
                 if os.path.isfile('/home/pi/cat-laser/src/stop-script'):
                     return
-            laser.tilt.angle = 11.5
+            laser.tilt.angle = 14
             laser.pan.angle = 110
             start_time = time.time() + random.randint(1200, 5400)
             while time.time() < start_time:
@@ -80,7 +80,7 @@ except KeyboardInterrupt:
 These tilt and pan angles put the laser in a place that my cat cant see it, so when I stop
 the toy the laser is out of the way. When I get a 5v relay, Ill turn off the laser instead.
 """
-laser.tilt.angle = 11.5
+laser.tilt.angle = 14
 laser.pan.angle = 110
 if os.path.isfile('/home/pi/cat-laser/src/stop-script'):
     os.system("sudo -u root -S rm /home/pi/cat-laser/src/stop-script")
