7938f886d02f36f068d9872925658bb227751599
benfugate <benjamin_fugate13@yahoo.com>
2022-01-26 20:40:41 -0500

    create debug settings

--- src/laser.py
+++ src/laser.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
 import os
+import sys
 import time
 import random
 import numpy as np
+import traceback
 from adafruit_servokit import ServoKit
 import RPi.GPIO as GPIO
-
-GPIO.setup(17, GPIO.OUT)
-off = 0
-on = 1
 
 class Laser:
     def __init__(self):
         """
-        move_delay_seconds: how often in seconds the laser will take a chance to move to a new location
+        delay_between_movements: how often in seconds the laser will take a chance to move to a new location
         percentage_move_chance: percentage chance that the laser will move
         pan_range: angle range that the pan servo will move between
         tilt_range: tilt range that the tilt servo will move between
         """
-        self.move_delay_seconds = 1
+        self.delay_between_movements = 0
+        #self.sleep_time_range = (1200, 5400)
+        self.sleep_time_range = (5, 30)
         self.percentage_move_chance = 0.50
         self.pan_range = (0, 120)
         self.tilt_range = (80, 130)
 
         self.last_pan = 0
         self.last_tilt = 0
+
+        GPIO.setup(17, GPIO.OUT)
+        self.laser_off = 0
+        self.laser_on = 1
 
         kit = ServoKit(channels=16)
         self.tilt = kit.servo[0]
@@ -46,10 +50,10 @@ class Laser:
         time.sleep(0.03)  # give servos a chance to move
 
     def run(self):
-        print(f"Movement chance:\n    {self.percentage_move_chance*100}% every {self.move_delay_seconds} second")
+        print(f"Movement chance:\n    {self.percentage_move_chance*100}% every {self.delay_between_movements} second")
         time.sleep(3)
         while True:
-            GPIO.output(17, on)
+            GPIO.output(17, self.laser_on)
             print("turning on")
             on_time = time.time() + 900
             while time.time() < on_time:
@@ -59,12 +63,12 @@ class Laser:
                     pan = random.randint(self.pan_range[0], self.pan_range[1])
                     tilt = random.randint(self.tilt_range[0], self.tilt_range[1])
                     self.create_laser_path(pan, tilt)
-                #time.sleep(self.move_delay_seconds)
+                time.sleep(self.delay_between_movements)
                 if os.path.isfile('/home/pi/cat-laser/src/stop-script'):
                     return
-            GPIO.output(17, off)
+            GPIO.output(17, self.laser_off)
             print("turning off for a break")
-            start_time = time.time() + random.randint(1200, 5400)
+            start_time = time.time() + random.randint(self.sleep_time_range[0], self.sleep_time_range[1])
             while time.time() < start_time:
                 time.sleep(5)
                 if os.path.isfile('/home/pi/cat-laser/src/start-script'):
@@ -86,18 +90,25 @@ try:
                 os.system("sudo -u root -S rm /home/pi/cat-laser/src/start-script")
             if os.path.isfile('/home/pi/cat-laser/src/stop-script'):
                 os.system("sudo -u root -S rm /home/pi/cat-laser/src/stop-script")
-            GPIO.output(17, off)
+            GPIO.output(17, laser.laser_off)
     else:
-        exit(0)
+        errors_dir = f'{os.getcwd()}/errors/'
+        if not os.path.exists(errors_dir):
+            os.makedirs(errors_dir)
+
+        list_of_files = os.listdir(errors_dir)
+        full_path = ["{0}/{1}".format(errors_dir, x) for x in list_of_files]
+        if len(list_of_files) == 5:
+            oldest_file = min(full_path, key=os.path.getctime)
+            os.remove(oldest_file)
+        with open(f"{errors_dir}/exception-{int(time.time())}.txt", "w") as errorfile:
+            e_type, e_val, e_tb = sys.exc_info()
+            traceback.print_exception(e_type, e_val, e_tb, file=errorfile)
 except KeyboardInterrupt:
     os.system("sudo -u root -S rm /home/pi/cat-laser/src/active")
     print("Goodbye!")
 
-"""
-These tilt and pan angles put the laser in a place that my cat cant see it, so when I stop
-the toy the laser is out of the way. When I get a 5v relay, Ill turn off the laser instead.
-"""
-GPIO.output(17, off)
+GPIO.output(17, laser.laser_off)
 print("laser off, done")
 if os.path.isfile('/home/pi/cat-laser/src/active'):
     os.system("sudo -u root -S rm /home/pi/cat-laser/src/active")
